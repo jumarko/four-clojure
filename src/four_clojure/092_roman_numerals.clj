16d8c2ca6d84c82525f28371b5b85429f421052c
Juraj Martinka <jumarko@gmail.com>
2018-11-22 09:55:44 +0100

    Problem 92: Roman Numerals - complete solution using loop-recur.

--- src/four_clojure/092_roman_numerals.clj
+++ src/four_clojure/092_roman_numerals.clj
@@ -16,14 +16,30 @@
              \D 500
              \M 1000})
 
-(defn- roman-numberals [str-num]
-  (->> str-num
-       (str/upper-case)
-       (map romans)
-       (reduce + 0))
-  )
+(defn roman-numberals [str-num]
+  (let [numbers (->> str-num
+                     (str/upper-case)
+                     (map romans))]
+    (loop [total 0
+           nums numbers]
+      (let [[fst snd] nums]
+        (cond
+          (and fst snd)
+          (if (< fst snd)
+            ;; this is subtraction pattern
+            (recur (+ total (- snd fst)) (nnext nums))
+            ;; add only the first one because the second one can form subtraction pattern with the third element
+            (recur (+ total fst) (next nums)))
+
+          fst
+          (+ total fst)
+
+          :else
+          total)))))
 
 (deftest simple-without-subtraction
+  (testing "none"
+    (is (= 0 (roman-numberals ""))))
   (testing "one"
     (is (= 1 (roman-numberals "I"))))
   (testing "two"
@@ -40,3 +56,23 @@
     (is (= 8 (roman-numberals "VIII"))))
   (testing "longer"
     (is (= 1888 (roman-numberals "MDCCCLXXXVIII")))))
+
+(deftest subtraction
+  (testing "four"
+    (is (= 4 (roman-numberals "IV"))))
+  (testing "nine"
+    (is (= 9 (roman-numberals "IX"))))
+  (testing "longer"
+    (is (= 999 (roman-numberals "CMXCIX")))))
+
+
+(deftest official-tests
+  (testing "simple with subtraction"
+    (is (= 14 (roman-numberals "XIV"))))
+  (testing "longer but no subtraction"
+    (is (= 827 (roman-numberals "DCCCXXVII"))))
+  (testing "Long and subtraction"
+    (is (= 3999 (roman-numberals "MMMCMXCIX"))))
+  (testing "Subtraction from L"
+    (is (= 48 (roman-numberals "XLVIII")))))
+
