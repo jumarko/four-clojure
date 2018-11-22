353503a9178a005b15598a0d1a1025b0d554d466
Juraj Martinka <jumarko@gmail.com>
2018-11-22 10:10:43 +0100

    Problem 92: Roman Numerals - add adereth's clever solution.

--- src/four_clojure/092_roman_numerals.clj
+++ src/four_clojure/092_roman_numerals.clj
@@ -5,20 +5,18 @@
   and follow the subtractive principle: https://en.wikipedia.org/wiki/Roman_numerals#Subtractive_principle
   You don't need to handle any numbers greater than MMMCMXCIX (3999),
   the largest number representable with ordinary letters."
-  (:require [clojure.test :refer [deftest testing is]]
-            [clojure.string :as str]))
+  (:require [clojure.test :refer [deftest testing is]]))
 
-(def romans {\I 1
-             \V 5
-             \X 10
-             \L 50
-             \C 100
-             \D 500
-             \M 1000})
-
-(defn roman-numberals [str-num]
-  (let [numbers (->> str-num
-                     (str/upper-case)
+(defn roman-numerals [str-num]
+  (let [romans {\I 1
+                \V 5
+                \X 10
+                \L 50
+                \C 100
+                \D 500
+                \M 1000}
+        numbers (->> str-num
+                     (clojure.string/upper-case)
                      (map romans))]
     (loop [total 0
            nums numbers]
@@ -37,42 +35,51 @@
           :else
           total)))))
 
+;; adereth's solution:
+(defn roman-numerals [s]
+  (->> s
+       reverse
+       (replace (zipmap "MDCLXVI" [1000 500 100 50 10 5 1]))
+       (partition-by identity)
+       (map (partial apply +))
+       (reduce #((if (< %1 %2) + -) %1 %2))))
+
 (deftest simple-without-subtraction
   (testing "none"
-    (is (= 0 (roman-numberals ""))))
+    (is (= 0 (roman-numerals ""))))
   (testing "one"
-    (is (= 1 (roman-numberals "I"))))
+    (is (= 1 (roman-numerals "I"))))
   (testing "two"
-    (is (= 2 (roman-numberals "II"))))
+    (is (= 2 (roman-numerals "II"))))
   (testing "three"
-    (is (= 3 (roman-numberals "III"))))
+    (is (= 3 (roman-numerals "III"))))
   (testing "five"
-    (is (= 5 (roman-numberals "V"))))
+    (is (= 5 (roman-numerals "V"))))
   (testing "six"
-    (is (= 6 (roman-numberals "VI"))))
+    (is (= 6 (roman-numerals "VI"))))
   (testing "seven"
-    (is (= 7 (roman-numberals "VII"))))
+    (is (= 7 (roman-numerals "VII"))))
   (testing "eight"
-    (is (= 8 (roman-numberals "VIII"))))
+    (is (= 8 (roman-numerals "VIII"))))
   (testing "longer"
-    (is (= 1888 (roman-numberals "MDCCCLXXXVIII")))))
+    (is (= 1888 (roman-numerals "MDCCCLXXXVIII")))))
 
 (deftest subtraction
   (testing "four"
-    (is (= 4 (roman-numberals "IV"))))
+    (is (= 4 (roman-numerals "IV"))))
   (testing "nine"
-    (is (= 9 (roman-numberals "IX"))))
+    (is (= 9 (roman-numerals "IX"))))
   (testing "longer"
-    (is (= 999 (roman-numberals "CMXCIX")))))
+    (is (= 999 (roman-numerals "CMXCIX")))))
 
 
 (deftest official-tests
   (testing "simple with subtraction"
-    (is (= 14 (roman-numberals "XIV"))))
+    (is (= 14 (roman-numerals "XIV"))))
   (testing "longer but no subtraction"
-    (is (= 827 (roman-numberals "DCCCXXVII"))))
+    (is (= 827 (roman-numerals "DCCCXXVII"))))
   (testing "Long and subtraction"
-    (is (= 3999 (roman-numberals "MMMCMXCIX"))))
+    (is (= 3999 (roman-numerals "MMMCMXCIX"))))
   (testing "Subtraction from L"
-    (is (= 48 (roman-numberals "XLVIII")))))
+    (is (= 48 (roman-numerals "XLVIII")))))
 
