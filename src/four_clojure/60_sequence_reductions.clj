5657d87cbd0cfe36b464779906507b85c3123865
Juraj Martinka <jumarko@gmail.com>
2018-11-29 14:28:56 +0100

    60 Sequence Reductions - lazy solution.

--- src/four_clojure/60_sequence_reductions.clj
+++ src/four_clojure/60_sequence_reductions.clj
@@ -4,27 +4,22 @@
   It must accept 2 or 3 args and return a lazy sequence."
   (:require [clojure.test :refer [deftest testing is]]))
 
-
+;;; copied from `reductions` just removed calling `reduced?`
+;;; then copied from pcl's solution: http://www.4clojure.com/problem/solutions/60
 (defn my-reductions
   ([f coll]
-   (my-reductions (first coll) (rest coll)))
+   (my-reductions f (first coll) (rest coll)))
   ([f init coll]
-   (loop [results [init]
-          remaining-items coll]
-     (let [last-result (peek results)
-           next-item (first remaining-items)]
-       (if next-item
-         (recur
-          (conj results
-                (f last-result next-item))
-          (rest remaining-items))
-         results)))))
+   (cons init
+         (lazy-seq
+          (when-let [s (seq coll)]
+            (my-reductions f (f init (first s)) (rest s)))))))
 
-#_(my-reductions + 0 [1 2 3])
-
+;; try if it's lazy
+#_(take 5 (my-reductions + (range)))
 
 (deftest official-tests
-  #_(testing "lazy"
+  (testing "lazy"
     (is (= [0 1 3 6 10]
            (take 5 (my-reductions + (range))))))
   (testing "init value"
@@ -34,3 +29,4 @@
     (is (= 120
            (reduce * 2 [3 4 5])
            (last (my-reductions * 2 [3 4 5]))))))
+
