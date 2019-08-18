327247625a859846f8552ecdf92af7876fb45619
Juraj Martinka <jumarko@gmail.com>
2019-08-18 06:19:38 +0200

    Update to clojure 1.10.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject four-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "My solutions to 4clojure.com problems."
+  :url "https://github.com/jumarko/four-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot four-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
